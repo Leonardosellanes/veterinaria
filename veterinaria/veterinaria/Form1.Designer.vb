b1c65cdc0da17d628c65af5f5dfb02f4e85c6e58
Leonardosellanes <63883232+Leonardosellanes@users.noreply.github.com>
2020-05-12 16:06:59 -0300

    Add files via upload

---
+++ veterinaria/veterinaria/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.grdMascotas = New System.Windows.Forms.DataGridView()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtRaza = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtColor = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.grdMascotas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("HoloLens MDL2 Assets", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(268, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(187, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Registro de Mascotas"
+        '
+        'grdMascotas
+        '
+        Me.grdMascotas.AllowUserToOrderColumns = True
+        Me.grdMascotas.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grdMascotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdMascotas.Location = New System.Drawing.Point(378, 97)
+        Me.grdMascotas.Name = "grdMascotas"
+        Me.grdMascotas.Size = New System.Drawing.Size(481, 446)
+        Me.grdMascotas.TabIndex = 1
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtNombre.Location = New System.Drawing.Point(63, 36)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(255, 20)
+        Me.txtNombre.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nombre:"
+        '
+        'txtRaza
+        '
+        Me.txtRaza.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtRaza.Location = New System.Drawing.Point(63, 73)
+        Me.txtRaza.Name = "txtRaza"
+        Me.txtRaza.Size = New System.Drawing.Size(255, 20)
+        Me.txtRaza.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Raza:"
+        '
+        'txtColor
+        '
+        Me.txtColor.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtColor.Location = New System.Drawing.Point(63, 112)
+        Me.txtColor.Name = "txtColor"
+        Me.txtColor.Size = New System.Drawing.Size(255, 20)
+        Me.txtColor.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 115)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Color"
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.Location = New System.Drawing.Point(231, 201)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(87, 24)
+        Me.btnIngresar.TabIndex = 8
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.txtID)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
+        Me.GroupBox1.Controls.Add(Me.btnIngresar)
+        Me.GroupBox1.Controls.Add(Me.txtColor)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtRaza)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 172)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(328, 245)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(13, 202)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(83, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Dar de Alta"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(117, 202)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 24)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Actualizar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtID
+        '
+        Me.txtID.Enabled = False
+        Me.txtID.Location = New System.Drawing.Point(63, 158)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(33, 20)
+        Me.txtID.TabIndex = 11
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(270, 138)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(48, 20)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Limpiar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(860, 545)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.grdMascotas)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Mascotas"
+        CType(Me.grdMascotas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents grdMascotas As DataGridView
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtRaza As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtColor As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents Button3 As Button
+End Class
---
+++ veterinaria/veterinaria/Form1.vb
@@ -0,0 +1,154 @@
+﻿Imports MySql.Data.MySqlClient
+'Leonardo Sellanes
+Public Class Form1
+    Dim conexion As MySqlConnection = New MySqlConnection
+    Dim cmd As New MySqlCommand
+    Sub ActualizarSelect()
+
+        Dim ds As DataSet = New DataSet
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+
+        conexion.ConnectionString = "server=localhost; database=veterinaria; uid=root;pwd=;"
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "SELECT * FROM perros ORDER BY Nombre ASC"
+            adaptador.SelectCommand = cmd
+            adaptador.Fill(ds, "Tabla")
+            grdMascotas.DataSource = ds
+            grdMascotas.DataMember = "Tabla"
+
+            conexion.Close()
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+
+        If (txtNombre.Text <> "") And (txtRaza.Text <> "") And (txtColor.Text <> "") Then
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "INSERT INTO perros(Nombre,Raza,Color) VALUES (@Nombre, @Raza, @Color)"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text)
+            cmd.Parameters.AddWithValue("@Raza", txtRaza.Text)
+            cmd.Parameters.AddWithValue("@Color", txtColor.Text)
+            cmd.ExecuteNonQuery()
+
+            conexion.Close()
+            txtNombre.Clear()
+            txtRaza.Clear()
+            txtColor.Clear()
+
+            ActualizarSelect()
+        Else
+            MsgBox("¡Debe ingresar Todos los datos Solicitados!")
+        End If
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ActualizarSelect()
+
+    End Sub
+
+    Private Sub grdMascotas_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles grdMascotas.CellContentClick
+
+    End Sub
+    Private Sub grdMascotas_SelectionChanged(sender As Object, e As EventArgs) Handles grdMascotas.SelectionChanged
+        If (grdMascotas.SelectedRows.Count > 0) Then
+            txtNombre.Text = grdMascotas.Item("Nombre", grdMascotas.SelectedRows(0).Index).Value
+            txtRaza.Text = grdMascotas.Item("Raza", grdMascotas.SelectedRows(0).Index).Value
+            txtColor.Text = grdMascotas.Item("Color", grdMascotas.SelectedRows(0).Index).Value
+            txtID.Text = grdMascotas.Item("id", grdMascotas.SelectedRows(0).Index).Value
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If (txtNombre.Text <> "") And (txtRaza.Text <> "") And (txtColor.Text <> "") Then
+            conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+
+            Try
+                conexion.Open()
+                cmd.Connection = conexion
+
+                cmd.CommandText = "UPDATE perros SET Nombre=@Nombre, Raza=@Raza, Color=@Color WHERE id=@id"
+                cmd.Prepare()
+
+                cmd.Parameters.Clear()
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text)
+                cmd.Parameters.AddWithValue("@Raza", txtRaza.Text)
+                cmd.Parameters.AddWithValue("@Color", txtColor.Text)
+                cmd.Parameters.AddWithValue("@id", txtID.Text)
+                cmd.ExecuteNonQuery()
+                txtNombre.Clear()
+                txtColor.Clear()
+                txtRaza.Clear()
+                txtID.Clear()
+
+                conexion.Close()
+
+                ActualizarSelect()
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        Else
+            MsgBox("¡Debe seleccionar una mascota y modificar sus datos antes de proceder!")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        conexion.ConnectionString = "server=localhost; database=veterinaria;Uid=root;Pwd=;"
+        If MessageBox.Show("¿Desea dar de Alta esta mascota?,Sera eliminado de el registro", "Alta", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) = DialogResult.OK Then
+
+        End If
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "DELETE FROM perros WHERE id=@id"
+            cmd.Prepare()
+
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@id", txtID.Text)
+            cmd.ExecuteNonQuery()
+            txtNombre.Clear()
+            txtColor.Clear()
+            txtRaza.Clear()
+            txtID.Clear()
+
+            conexion.Close()
+
+            ActualizarSelect()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (txtNombre.Text <> "") Or (txtRaza.Text <> "") Or (txtColor.Text <> "") Then
+            txtNombre.Clear()
+            txtRaza.Clear()
+            txtColor.Clear()
+            txtID.Clear()
+        Else
+            MsgBox("Los campos ya se encuntran vacios")
+        End If
+
+
+    End Sub
+End Class
